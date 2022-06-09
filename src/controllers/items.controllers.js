ae0f2a63a0b541eca730267b9be45e9b10188d52
alerebal <ale.rebal@gmail.com>
2022-06-09 08:10:36 +0100

    Add getItems and getItem functions

--- src/controllers/items.controllers.js
+++ src/controllers/items.controllers.js
@@ -1,14 +1,11 @@
 const itemsCtrl = {};
 const Item = require('../models/Item');
 
-itemsCtrl.getItems = async (req, res) => {
-    console.log('Connected')
-}
 
 // Create an Item
 itemsCtrl.createItem = async (req, res) => {
     try {
-        const {name, description, price, qty} = req.body;
+        const { name, description, price, qty } = req.body;
         const item = new Item({
             name, description, price, qty
         })
@@ -16,8 +13,31 @@ itemsCtrl.createItem = async (req, res) => {
         return res.json(item)
     } catch (error) {
         const err = error.message.split(': ')[2]
-        return res.json({'Error': err})
+        return res.status(400).json({ Error: err })
     }
 }
+
+// Retrieve all the items
+itemsCtrl.getItems = async (req, res) => {
+    try {
+        const items = await Item.find()
+        return res.json({ 'Items': items })
+    } catch (error) {
+        return res.status(404).json({ Error: 'Users not found' })
+    }
+}
+
+// Retrieve an Item by id
+itemsCtrl.getItem = async (req, res) => {
+    try {
+        const { id } = req.params
+        const item = await Item.findById(id)
+        return res.status(200).json(item)
+    } catch (error) {
+        return res.status(404).json({ Error: 'User not found' })
+    }
+}
+
+
 
 module.exports = itemsCtrl
--- src/routes/items.routes.js
+++ src/routes/items.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const {
     getItems,
+    getItem,
     createItem
 } = require('../controllers/items.controllers')
 
-router.get('/', getItems)
+router.get('/items', getItems)
+router.get('/item/:id', getItem)
 
 router.post('/createItem', createItem)
 
